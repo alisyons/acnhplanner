fc82dcfa3fffcf97ec59ab621fa3ded640640e5f
Alisa <56443688+alisyons@users.noreply.github.com>
2020-04-25 19:11:57 +0200

    Fixed routing

--- acnhplanner/src/app/app-routing.module.ts
+++ acnhplanner/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {WishlistComponent} from './wishlist/wishlist.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 
 const routes: Routes = [
@@ -10,12 +12,19 @@ const routes: Routes = [
   },
   {
     path: 'home', component: HomeComponent
+  },
+  {
+    path: 'wishlist', component: WishlistComponent
   }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
+    BrowserModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
